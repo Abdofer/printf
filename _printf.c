8e63bca339d12b08b75e9fa6c4422ab3b9068775
Abdofer <abderrazaq.fermaq@gmail.com>
2023-04-27 09:33:31 -0700

    solution to thevarious tasks

--- _printf.c
+++ _printf.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-void print_buffer(char buff[], int *buff_index);
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printing function
- * @format: format
- * Return: Printed input
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	int i, prntd = 0, printed_chars = 0;
-	int flag, width, precision, size, buff_index = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buff[BUFFER_SIZE];
+	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
@@ -24,27 +23,29 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			buff[buff_index++] = format[i];
-			if (buff_index == BUFFER_SIZE)
-				print_buffer(buff, &buff_index);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buff, &buff_index);
-			flag = get_flag(format, &i);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			prntd = handler_print(format, &i, list, buff, flag, width, precision, size);
-			if (prntd == -1)
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			printed_chars += prntd;
+			printed_chars += printed;
 		}
 	}
 
-	print_buffer(buff, &buff_index);
+	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
 
@@ -52,15 +53,14 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the content of the buffer
- * @buff: Array of chars
- * @buff_index: Index of the buffer
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-
-void print_buffer(char buff[], int *buff_index)
+void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_index > 0)
-		write(1, &buff[0], *buff_index);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-	*buff_index = 0;
+	*buff_ind = 0;
 }
--- get_size.c
+++ get_size.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_size - Calculates the size to cast the argument.
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
  *
- * Return: Precision
+ * Return: Precision.
  */
 int get_size(const char *format, int *i)
 {
@@ -13,9 +13,9 @@ int get_size(const char *format, int *i)
 	int size = 0;
 
 	if (format[curr_i] == 'l')
-		size = SIZE_LONG;
+		size = S_LONG;
 	else if (format[curr_i] == 'h')
-		size = SIZE_SHORT;
+		size = S_SHORT;
 
 	if (size == 0)
 		*i = curr_i - 1;
